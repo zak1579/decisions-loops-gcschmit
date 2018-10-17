fee604fd1ed86a49c25cee19cbf69400127e598f
Geoff Schmit <gcschmit@gmail.com>
2018-10-17 13:40:30 -0500

    added string comparison notes

--- Chapter 5 Notes/Notes.java
+++ Chapter 5 Notes/Notes.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Write a description of class Notes here.
@@ -101,6 +102,81 @@ public class Notes
         
         // don't have to use an if statement
         //return (Math.abs(num1 - num2) < EPSILON);
+    }
+    
+    public static void stringExample()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter two words: ");
+        String str1 = s.next();
+        String str2 = s.next();
+        
+        /*
+         * The equality operator (==) returns true if the two
+         *      variables (e.g., str1 and str2) contain the same
+         *      value. For variables of a class type, including
+         *      Strings, this means they contain the same reference.
+         *      That is, they refer to the same object in memory,
+         *      not that two Strings have the same sequence of
+         *      characters.
+         */
+        if(str1 == str2)
+        {
+            System.out.println("string references are equal");
+        }
+        else
+        {
+            System.out.println("string references are not equal");
+        }
+        
+        /*
+         * The equals method returns true if the two objects
+         *      referenced by the variables are "equal". What
+         *      equals means is defined by class. For Strings,
+         *      it means that the two objects have the same
+         *      sequence of characters.
+         */
+        if(str1.equals(str2))
+        {
+            System.out.println("strings are equal");
+        }
+        else
+        {
+            System.out.println("strings are not equal");
+        }
+        
+        /*
+         * We will determine which string comes first lexographically
+         *      using the compareTo method of the String class.
+         *      
+         *  compareTo returns an int:
+         *      0:          if the strings are equal (same sequence
+         *                      of characters)
+         *      < 0:        if str1 < str2 lexographically
+         *      > 0:        if str1 > str2 lexographically
+         */
+        int result = str1.compareTo(str2);
+        String firstStr = null;
+        
+        if(result < 0)
+        {
+            firstStr = str1;
+        }
+        else if(result > 0)
+        {
+            firstStr = str2;
+        }
+        
+        if(firstStr != null)
+        {
+            System.out.println("The first string is: " + firstStr);
+            System.out.println("Its length is: " + firstStr.length());
+        }
+        else
+        {
+            System.out.println("strings are equal");
+        }
+        
     }
 }
 
