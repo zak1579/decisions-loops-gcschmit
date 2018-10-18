ed89a94abeedfe1fabe990b319e49c6bcbbd3dda
Geoff Schmit <gcschmit@gmail.com>
2018-10-18 10:30:26 -0500

    started notes on short circuit

--- Chapter 5 Notes/Notes.java
+++ Chapter 5 Notes/Notes.java
@@ -175,7 +175,32 @@ public class Notes
         else
         {
             System.out.println("strings are equal");
-        }       
+        }
+        
+        /*
+         * This is an example of a "short circuit".
+         *      If the left operand is false, the right operand
+         *      will not be evaluated because the AND operation
+         *      is already false.
+         */
+        if(firstStr != null && firstStr.length() > 3)
+        {
+            System.out.println("The first string has more than 3 characters.");
+        }
+        
+        /*
+         * This is another "short circuit" example.
+         *      If the left operand is true, the right operand
+         *      will not be evaluated because the OR operation
+         *      is already true.
+         */
+        System.out.print("Enter your two favorite fruits: ");
+        if(s.next().equals("kiwi") || s.next().equals("kiwi"))
+        {
+            System.out.println("Kiwi is one of my favorite fruits too!");
+        }
+        
+        
     }
     
     
