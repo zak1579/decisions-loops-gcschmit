a7e1e1548c1505282ef9afe777c41c1d70df5a87
Geoff Schmit <gcschmit@gmail.com>
2018-10-24 13:35:42 -0500

    notes for swtich and more operators

--- Chapter 5 Notes/Notes.java
+++ Chapter 5 Notes/Notes.java
@@ -209,6 +209,120 @@ public class Notes
         System.out.println("Favorite ice cream flavor: " + flavor);
         
     }
+    
+    public static String getStudentClass(int gradeNumber)
+    {
+        /*
+         * switch statement
+         *  
+         *  This is another conditional decision statement (Like
+         *      an if statement). It is preferred when evaluating
+         *      several *discrete* values. It supports byte, short,
+         *      char, and int primitive types. It also supports
+         *      enumerations and String objects.
+         *    
+         *  The condition is evaluated and then the flow of
+         *      execution jumps to the case that matches.
+         */
+        String studentClass = "";
+        
+        switch(gradeNumber)
+        {
+            case 9:
+            {
+                studentClass = "freshman";
+                
+                /*
+                 * break causes the flow of execution to leave the
+                 *      switch. Without a break, the flow of
+                 *      execution continues into the next case.
+                 */
+                break;
+            }
+            
+            case 10:
+            {
+                studentClass = "sophomore";
+                break;
+            }
+            
+            case 11:
+            {
+                studentClass = "junior";
+                break;
+            }
+            
+            case 12:
+            {
+                studentClass = "senior";
+                break;
+            }
+            
+            /*
+             * By leaving out the break, multiple cases can run
+             *      tehe same code.
+             */
+            case 6:
+            case 7:
+            case 8:
+            {
+                studentClass = "junior high";
+                break;
+            }
+            
+            /*
+             * default matches everything not matched by one of
+             *      the cases
+             */
+            default:
+            {
+                studentClass = "elementary";
+                break;
+            }
+        }
+        
+        return studentClass;
+    }
+    
+    public static void extraOperatorsExample()
+    {
+        /*
+         * augmented assignment operators: +=, -=, *=, /=, %=
+         *  These are the same as in Python.
+         *  Perform the specified mathematical operation to the
+         *      variable and then assign the resulting value to
+         *      the same variable.
+         */
+        int x = 7;
+        int y = 7;
+        int z = 7;
+        
+        x = x + 1;
+        y += 1;
+        z++;
+        
+        System.out.println("x = " + x + "; y = " + y + "; z = " + z);
+        
+        /*
+         * post increment/decrement operators (++/--)
+         *      These are equivalent to adding/subtracting 1 from
+         *          the variable.
+         *      These return the value before performing the
+         *          increment/decrement.
+         */
+        int a = 7;
+        int b = a++;
+        System.out.println("a = " + a + "; b = " + b); // a = 8; b = 7
+        
+        /*
+         * pre increment/decrement operators (++/--)
+         *      These return the value after performing the
+         *          increment/decrement.
+         */
+        int c = 7;
+        int d = ++c;
+        System.out.println("c = " + c + "; d = " + d); // c = 8; d = 8
+    }
 }
 
 
